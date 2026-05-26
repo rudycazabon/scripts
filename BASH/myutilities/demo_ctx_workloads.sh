f367a2755a1b9f7d1bc84137871171a9be4317ab
Rudy Cazabon <rudycazabon@gmail.com>
2026-05-26 16:56:51 -0700

    Add SCRIPT_DIR shortcut and build_script.sh

--- BASH/myutilities/demo_ctx_workloads.sh
+++ BASH/myutilities/demo_ctx_workloads.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
-source "./logger.sh"
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+source "$SCRIPT_DIR/logger.sh"
 
 do_work() {
     echo
